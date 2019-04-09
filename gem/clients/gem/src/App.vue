197ecf1557e4aea7ba982f69a9c4aaa1e5eea45f
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-04-09 16:11:23 +0300

    vscode workspace file

--- gem/clients/gem/src/App.vue
+++ gem/clients/gem/src/App.vue
@@ -23,7 +23,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 import { Auth } from "@/modules/auth/store/auth";
 
 @Component
