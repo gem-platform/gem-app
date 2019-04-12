eb569a96e5af17f30a2643cdfcaddeca418ff9ca
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-04-12 18:43:53 +0300

    components decomposition

---
+++ gem/clients/gem/src/modules/admin/components/EditDialog.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-dialog
+    v-model="visible"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <v-card data-ref="edit-user-dialog">
+      <v-toolbar dark color="primary" flat>
+        <v-btn icon dark @click="close" data-ref="close">
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-toolbar-title>Edit user</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn dark flat @click="save" data-ref="save-user">Save</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <slot />
+        </v-layout>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Emit, Prop, Model } from "vue-property-decorator";
+
+@Component
+export default class EditDialog extends Vue {
+  @Prop({ default: false }) visible: boolean;
+
+  @Emit("close") close() {
+    return;
+  }
+  @Emit("save") save() {
+    return;
+  }
+}
+</script>
--- gem/clients/gem/src/modules/admin/components/EditUserDialog.vue
+++ gem/clients/gem/src/modules/admin/components/EditUserDialog.vue
@@ -1,60 +1,39 @@
 <template>
-  <v-dialog
-    v-model="visible"
-    fullscreen
-    hide-overlay
-    transition="dialog-bottom-transition"
-  >
-    <v-card data-ref="edit-user-dialog">
-      <v-toolbar dark color="primary" flat>
-        <v-btn icon dark @click="close" data-ref="close">
-          <v-icon>close</v-icon>
-        </v-btn>
-        <v-toolbar-title>Edit user</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items>
-          <v-btn dark flat @click="save" data-ref="save-user">Save</v-btn>
-        </v-toolbar-items>
-      </v-toolbar>
-
-      <v-container grid-list-md>
-        <v-layout wrap>
-          <v-flex xs12>
-            <v-text-field
-              v-model="user.full_name"
-              label="Name"
-              required
-              ref="name"
-            ></v-text-field>
-          </v-flex>
-          <v-flex xs12>
-            <v-text-field
-              v-model="user.email"
-              label="Email"
-              ref="email"
-              required
-            ></v-text-field>
-          </v-flex>
-          <v-flex xs12>
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              type="password"
-              required
-            ></v-text-field>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </v-dialog>
+  <edit-dialog :visible="visible" @close="close" @save="save">
+    <v-flex xs12>
+      <v-text-field
+        v-model="user.full_name"
+        label="Name"
+        required
+        ref="name"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs12>
+      <v-text-field
+        v-model="user.email"
+        label="Email"
+        ref="email"
+        required
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs12>
+      <v-text-field
+        v-model="user.password"
+        label="Password"
+        type="password"
+        required
+      ></v-text-field>
+    </v-flex>
+  </edit-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Emit, Prop, Model } from "vue-property-decorator";
 import { User } from "@/modules/types.ts";
 import { AnonymousUser } from "../../types";
+import EditDialog from "./EditDialog.vue";
 
-@Component
+@Component({ components: { EditDialog } })
 export default class EditUserDialog extends Vue {
   @Prop({ default: AnonymousUser }) public readonly user!: User;
   @Prop({ default: false }) visible: boolean;
