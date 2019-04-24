60bdf91cb576feb8e3e8d418cf75692eea11b72a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-04-24 17:40:03 +0300

    fix change password

--- gem/clients/gem/src/modules/admin/services/users.ts
+++ gem/clients/gem/src/modules/admin/services/users.ts
@@ -10,7 +10,7 @@ export default class UsersService extends CrudService<IUser> {
 
   public async changePassword(user: IUser, password: string) {
     const url = this.url + "/" + user.oid + "/changePassword";
-    const res = await Axios.put(url, { password: "123" });
+    const res = await Axios.put(url, { password });
     return res.data;
   }
 }
