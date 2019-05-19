a5949df54e5e7b0d95f2b6bd4d45b4fb64cb1465
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2019-05-19 12:24:57 +0300

    DB migrations (#13)
    + Alembic skeleton
    + Create users migration
    + Wait for db before migrations

---
+++ gem/apps/gem/alembic/versions/2019-05-10_3f6b178d7e33_create_users_table.py
@@ -0,0 +1,30 @@
+"""create users table
+
+Revision ID: 3f6b178d7e33
+Revises:
+Create Date: 2019-05-10 16:03:01.897560
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "3f6b178d7e33"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "user",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.String(250), nullable=False),
+        sa.Column("full_name", sa.String(250), nullable=True),
+        sa.Column("email", sa.String(50), nullable=False),
+        sa.Column("hashed_password", sa.String(100), nullable=False),
+        sa.Column("disabled", sa.Boolean, nullable=False)
+    )
+
+
+def downgrade():
+    op.drop_table("user")
