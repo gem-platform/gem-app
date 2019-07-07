d3d47069f4c2031f989f50903b62cb392c945bd0
Advaita Krishna das <advaita.krishna.das@gmail.com>
2019-07-07 17:01:00 +0300

    covered by tests

--- gem/clients/gem/src/modules/auth/components/LocaleSwitch.vue
+++ gem/clients/gem/src/modules/auth/components/LocaleSwitch.vue
@@ -1,22 +1,25 @@
 <template>
   <fragment>
-    <v-btn icon large @click="changeLocale('en')">
+    <v-btn icon large @click="changeLocale('en')" ref="en">
       EN
     </v-btn>
-    <v-btn icon large @click="changeLocale('ru')">
+    <v-btn icon large @click="changeLocale('ru')" ref="ru">
       RU
     </v-btn>
   </fragment>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
 import { ICredentials } from "../types";
 
 @Component({})
 export default class LocaleSwitch extends Vue {
+  /** Sets locale of an applications. Emits 'localeChanged' event. */
+  @Emit("localeChanged")
   private changeLocale(locale: string) {
     this.$i18n.locale = locale;
+    return locale;
   }
 }
 </script>
